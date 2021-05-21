ce25806ebb3155fe68cd21859fc6e0284e2a04f2
viniciusmartins21 <viniciusmartins.12@hotmail.com>
2021-05-20 23:07:44 -0300

    exercicio 1 e 2 banco de dados

---
+++ lição 1.sql
@@ -0,0 +1,9 @@
+select * from tb_funcionarios;
+
+select * from tb_funcionarios where salario > 2000;
+
+select * from tb_funcionarios where salario < 2000;
+
+update tb_funcionarios set nome = "Marcelo" where id = 1;
+
+Select * from tb_funcionarios
---
+++ lição 2.sql
@@ -0,0 +1,6 @@
+select * from tb_produtos where valor > 500;
+select * from tb_produtos where valor < 500;
+
+update tb_produtos set nome = "chave de roda" where id = 1;
+
+select * from tb_produtos 
